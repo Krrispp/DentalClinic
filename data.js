6582d018709a730cafe6ed76a8d3ab73c67358c3
Krrispp <163117059+Krrispp@users.noreply.github.com>
2024-03-14 13:32:42 +0300

    Upload

--- data.js
+++ data.js
@@ -1,7 +1,45 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0--",
+      "id": "0---",
+      "name": "Вход с улицы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3406292008606684,
+        "pitch": 0.12564203403675833,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.012764557874531235,
+          "pitch": 0.08502077795085583,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
       "name": "Холл, ресепшен",
       "levels": [
         {
@@ -28,34 +66,34 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -0.0907775460130793,
-        "pitch": 0.004044053570098072,
+        "yaw": -0.011220011783819928,
+        "pitch": 0,
         "fov": 1.3616874290001844
       },
       "linkHotspots": [
         {
-          "yaw": -1.6802481558154945,
-          "pitch": 0.008586059538053803,
-          "rotation": 6.283185307179586,
-          "target": "1--1"
+          "yaw": 0.2534245759307492,
+          "pitch": 0.036943688132723196,
+          "rotation": 0,
+          "target": "3-"
         },
         {
-          "yaw": 0.26912500508158743,
-          "pitch": 0.04615470827038948,
+          "yaw": -1.6929068089351134,
+          "pitch": -0.008224327579419821,
           "rotation": 0,
-          "target": "2-"
+          "target": "2--1"
         },
         {
-          "yaw": -3.130962492569058,
-          "pitch": 0.04528802860996883,
+          "yaw": 3.119906729121751,
+          "pitch": 0.07500072865183327,
           "rotation": 0,
-          "target": "7---"
+          "target": "0---"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1--1",
+      "id": "2--1",
       "name": "Кабинет №1",
       "levels": [
         {
@@ -82,215 +120,23 @@ var APP_DATA = {
       ],
       "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -2.0257959828930794,
-        "pitch": 0.0668951277587162,
+        "yaw": -1.9652652933006074,
+        "pitch": 0,
         "fov": 1.3616874290001844
       },
       "linkHotspots": [
         {
-          "yaw": 1.2519299035859692,
-          "pitch": -0.10618091144097974,
+          "yaw": 1.246149177734189,
+          "pitch": -0.08241160236593004,
           "rotation": 0,
-          "target": "0--"
+          "target": "1--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-",
+      "id": "3-",
       "name": "Коридор",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": -0.3928223033504885,
-        "pitch": 0,
-        "fov": 1.3616874290001844
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.3040419806662626,
-          "pitch": 0.1473343336657411,
-          "rotation": 0,
-          "target": "0--"
-        },
-        {
-          "yaw": -1.5681818746263634,
-          "pitch": -0.0143051269972716,
-          "rotation": 0,
-          "target": "4--"
-        },
-        {
-          "yaw": 2.109907492767805,
-          "pitch": -0.02062326741599918,
-          "rotation": 0,
-          "target": "3-2"
-        },
-        {
-          "yaw": -0.5854286441082373,
-          "pitch": 0.012622309916068275,
-          "rotation": 0,
-          "target": "5-"
-        },
-        {
-          "yaw": 1.5592484988468733,
-          "pitch": -0.054244372759583825,
-          "rotation": 0,
-          "target": "6-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-2",
-      "name": "Кабинет2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 2.74055829275137,
-        "pitch": 0.017343181270778274,
-        "fov": 1.3616874290001844
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.3751575630028974,
-          "pitch": -0.0045326520123314396,
-          "rotation": 0,
-          "target": "2-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4--",
-      "name": "Кабинет-офис",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": -0.8460689137881605,
-        "pitch": 0,
-        "fov": 1.3616874290001844
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.7114729032447302,
-          "pitch": -0.07969061118881271,
-          "rotation": 0,
-          "target": "2-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-",
-      "name": "Санузел",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 4096,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.3616874290001844
-      },
-      "linkHotspots": [
-        {
-          "yaw": 3.1383687960318998,
-          "pitch": -0.06875449952511126,
-          "rotation": 0,
-          "target": "2-"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-",
-      "name": "Рентген",
       "levels": [
         {
           "tileSize": 256,
@@ -322,17 +168,41 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.481719267336416,
-          "pitch": 0.004115421515393791,
+          "yaw": -0.6123956791624856,
+          "pitch": 0.008241802237968443,
           "rotation": 0,
-          "target": "2-"
+          "target": "6-"
+        },
+        {
+          "yaw": -1.56832212437061,
+          "pitch": -0.07676050286214675,
+          "rotation": 0,
+          "target": "7--"
+        },
+        {
+          "yaw": 1.5626671493673463,
+          "pitch": -0.09699069260434712,
+          "rotation": 0,
+          "target": "5--"
+        },
+        {
+          "yaw": 2.1337393581879205,
+          "pitch": -0.11558492921231789,
+          "rotation": 0,
+          "target": "4--2"
+        },
+        {
+          "yaw": -2.2887860508510975,
+          "pitch": 0.059563572982467505,
+          "rotation": 0,
+          "target": "1--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7---",
-      "name": "Вход с улицы",
+      "id": "4--2",
+      "name": "Кабинет №2",
       "levels": [
         {
           "tileSize": 256,
@@ -350,30 +220,160 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
         }
       ],
-      "faceSize": 2048,
+      "faceSize": 4096,
       "initialViewParameters": {
-        "yaw": -0.4204984767118667,
-        "pitch": 0.04034085485544736,
+        "yaw": -2.8777355285847115,
+        "pitch": 0.32383563350400024,
         "fov": 1.3616874290001844
       },
       "linkHotspots": [
         {
-          "yaw": 0.011797001241152572,
-          "pitch": 0.07423378343318987,
+          "yaw": -0.3682138585035908,
+          "pitch": -0.02226009131483231,
           "rotation": 0,
-          "target": "0--"
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "Рентген-кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.0151466318755471,
+        "pitch": 0.06523397219773486,
+        "fov": 1.3616874290001844
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4970700340264997,
+          "pitch": -0.079584805758806,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "Санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0942375373077713,
+          "pitch": -0.0769544327496412,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "Кабинет-офис",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6970333839783356,
+          "pitch": -0.12848117020245198,
+          "rotation": 0,
+          "target": "3-"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Стоматология",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
